d047d9fdea632e6fe88bd162d99fe6fc49989b36
Sibusiso Vincent <51314855+vultureofficial@users.noreply.github.com>
2023-02-18 11:35:13 +0200

    Create hash.c

---
+++ src/hash.c
@@ -0,0 +1,18 @@
+#include <hash.h>
+#include <string.h>
+
+void hash(char *dest, const char *str) {
+    size_t size = strlen(str);
+    char _x[size + 1]; 
+    
+    for (int i = 0; i < size; i++) {
+        char c = abs(str[i] + (str[i] + 128)); 
+        if (c < 0) c *= -1; 
+        if (c < 30) c += 30;\
+        if (c == ' ') c += 65;
+        _x[i] = c; 
+    } 
+    //printf("Hashing %s -> %s\n", str, _x);
+    strcpy(dest, _x);  
+}
+
